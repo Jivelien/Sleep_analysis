f1a403a91944588f04c2f1c3ca39a4dfe09100c0
Jivelien <julien.delannoy@hotmail.com>
2024-12-09 18:58:05 +0100

    add last period to interface

--- signal_periodizer/interface.py
+++ signal_periodizer/interface.py
@@ -3,4 +3,7 @@ from typing import List
 
 class SignalPeriodizerInterface:
     def period_for(self, signal: List[int], n_of_period: int) -> List[float]:
-        raise NotImplementedError()
+        raise NotImplementedError
+
+    def last_period_for(self, signal: List[float]) -> int:
+        raise NotImplementedError
